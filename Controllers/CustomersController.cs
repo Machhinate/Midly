cc07b7dfe817cd5d39afdc2717818cd65ac9f64b
Micah <micah.mix+KRACK@gmail.com>
2020-01-30 18:34:04 +1300

    ex3 pt2

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -9,6 +9,7 @@ namespace Vidly.Controllers
     public class CustomersController : Controller
     {
         private ApplicationDbContext _context;
+
         public CustomersController()
         {
             _context = new ApplicationDbContext();
@@ -19,14 +20,14 @@ namespace Vidly.Controllers
         }
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            return View(customer);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
